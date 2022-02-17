3302bcf42e7a77b2e82bc86e1fcc6417f99b8270
Peter Kőrösi-Szabó <koszpe@gmail.com>
2022-02-17 10:36:29 +0100

    add multipredictor

--- simsiam/builder.py
+++ simsiam/builder.py
@@ -85,10 +85,40 @@ class MultiGroup(SimSiam):
         gs = []
         # p = torch.cat([z1, z2])
         p = torch.cat([p1, p2])
-        groups = self.group_head(p)
+        groups = self.group_head(p.detach())
         groups = AllGather.apply(groups)
         bs = groups.shape[0]
         for g_slice, g_size, g_num in zip(self.group_slices, self.group_sizes, self.group_nums):
             group = groups[:, g_slice].view(bs, g_num, g_size).permute(1, 0, 2).contiguous() # group_num x batch_size x group_size
             gs.append(group)
         return p1, p2, z1.detach(), z2.detach(), gs
+
+class MultiPredictor(nn.Module):
+
+    def __init__(self, dim, bottleneck_dims):
+        super().__init__()
+        self.predictors = dict()
+        assert len(set(bottleneck_dims)) == len(bottleneck_dims), "all bottleneck_dims must be unique"
+        for bn_dim in bottleneck_dims:
+            predictor = nn.Sequential(nn.Linear(dim, bn_dim, bias=False),
+                                           nn.BatchNorm1d(bn_dim),
+                                           nn.ReLU(inplace=True),  # hidden layer
+                                           nn.Linear(bn_dim, dim))  # output layer
+            setattr(self, f"predictor_{bn_dim}", predictor)
+            self.predictors[bn_dim] = predictor
+
+    def forward(self, x):
+        outs = dict()
+        for bn_dim, predictor in self.predictors.items():
+            outs[bn_dim] = predictor(x)
+        return outs
+
+class MultiPredictorSimSiam(SimSiam):
+    def __init__(self, bottleneck_dims, *args, **kwargs):
+        super(MultiPredictorSimSiam, self).__init__(*args, **kwargs)
+        self.bottleneck_dims = bottleneck_dims
+        self.predictor = MultiPredictor(self.dim, bottleneck_dims)
+
+    def forward(self, x1, x2):
+        p1, p2, z1, z2 = super(MultiPredictorSimSiam, self).forward(x1, x2)
+        return p1, p2, z1.detach(), z2.detach()
